0f427eaa6242eff04d78ea95bd755cb53832b4fd
Luka Knezic <luka@giscloud.com>
2025-02-22 01:17:34 +0100

    Fix leftovers

--- run.php
+++ run.php
@@ -37,18 +37,18 @@ do
     {
         $url = $proxy->curl($url_r, true);
         file_put_contents($curlOutputPath, $html);
-        sendToDiscord($webhook, "$tag_myself Script died, `302 Found` returned!\n[".date(DATE_RFC2822)."]\nSee output: $curlOutputUrl");
+        sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK'), Environment::getStringEnv('USER_ID') . " Script died, `302 Found` returned!\n[".date(DATE_RFC2822)."]\nSee output: $curlOutputUrl");
         die();
     } elseif (strstr($html, "validate.perfdrive.com/") && strstr($html, "/captcha")) {
         file_put_contents($curlOutputPath, $html);
-        sendToDiscord($webhook, "$tag_myself Proxy failed, captcha triggered!!!\n[".date(DATE_RFC2822)."]\nSee output: $curlOutputUrl");
+        sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK'), Environment::getStringEnv('USER_ID') . " Proxy failed, captcha triggered!!!\n[".date(DATE_RFC2822)."]\nSee output: $curlOutputUrl");
         die();
     } elseif (strstr($html, "Error 400")) {
         file_put_contents($curlOutputPath, $html);
-        sendToDiscord($webhook, "$tag_myself Script died, `Error 400 (Bad Request)` returned!\n[".date(DATE_RFC2822)."]\nSee output: $curlOutputUrl");
+        sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK'), Environment::getStringEnv('USER_ID') . " Script died, `Error 400 (Bad Request)` returned!\n[".date(DATE_RFC2822)."]\nSee output: $curlOutputUrl");
         die();
     } else {
-        sendToDiscord($webhook_ping, "Normal response returned [".date(DATE_RFC2822)."]");
+        sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK_PING'), "Normal response returned [".date(DATE_RFC2822)."]");
     }
     $html = HtmlDomParser::str_get_html($html);
     $i = 0;
@@ -89,7 +89,7 @@ do
 file_put_contents("seen.json", json_encode($seen, JSON_PRETTY_PRINT));
 
 echo "\e[1;33mDone, found $new new. \nRun \e[0;32mphp seen.php\e[1;33m to get a full list.\e[0m\n";
-sendToDiscord($webhook_ping, "Found $new new items [".date(DATE_RFC2822)."]");
+sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK_PING'), "Found $new new items [".date(DATE_RFC2822)."]");
 
 if ($new > 0)
 {
@@ -99,9 +99,9 @@ if ($new > 0)
     if ($new > 5) {
         $groups = array_chunk(explode("\n", $newText), 5);
         foreach ($groups as $group) {
-            sendToDiscord($webhook, "New stuff [".date(DATE_RFC2822)."]:\n" . implode("\n", $group));
+            sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK'), "New stuff [".date(DATE_RFC2822)."]:\n" . implode("\n", $group));
         }
     } else {
-        sendToDiscord($webhook, "New stuff [".date(DATE_RFC2822)."]:\n".$newText);
+        sendToDiscord(Environment::getStringEnv('DISCORD_WEBHOOK'), "New stuff [".date(DATE_RFC2822)."]:\n".$newText);
     }
 }
